6eb874c37e6273a742abe564b5fe57d533687334
Dominik Grodl <dominikgrodl@icloud.com>
2024-02-19 12:55:19 +0100

    Remove, leave only ObservingUIViewController

--- Sources/ObservationExtras/ObservingViewController.swift
+++ Sources/ObservationExtras/ObservingViewController.swift
@@ -1,46 +1,13 @@
 #if canImport(UIKit)
 import UIKit
 
-open class ObservingViewController: UIViewController {
+open class ObservingUIViewController: UIViewController {
     open override func viewDidLoad() {
         super.viewDidLoad()
         observeState()
     }
     
     open func observeState() {}
-}
-
-open class ObservingUIViewController: ObservingViewController {
-    open override func viewDidLoad() {
-        super.viewDidLoad()
-        observeState()
-    }
-    
-    open override func observeState() {
-        super.observeState()
-    }
-}
-
-open class ObservingUITableViewController: ObservingViewController {
-    open override func viewDidLoad() {
-        super.viewDidLoad()
-        observeState()
-    }
-    
-    open override func observeState() {
-        super.observeState()
-    }
-}
-
-open class ObservingUICollectionViewController: ObservingViewController {
-    open override func viewDidLoad() {
-        super.viewDidLoad()
-        observeState()
-    }
-    
-    open override func observeState() {
-        super.observeState()
-    }
 }
 
 #endif
